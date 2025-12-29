9e13fd15813f7875f042085d4f8742290770cdaa
Vamsi Addanki <vamsiaddanki666@gmail.com>
2025-12-28 22:43:07 -0500

    generate collectives

---
+++ synthesis/generate-collectives.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+# Few examples:
+# python3 generate-collective.py 4x4 67108864 4 reduce-scatter-rd-nd out.json
+# python3 generate-collective.py 27 81 bruckallgather-r3-p3 out.json
+# python3 generate-collective.py 8 67108864 binomial-broadcast out.json
+# python3 generate-collective.py 4x4 67108864 1 reduce-scatter-swing-nd out.json
+# python3 generate-collective.py 4x4 67108864 4 reduce-scatter-swing-nd out.json
+
