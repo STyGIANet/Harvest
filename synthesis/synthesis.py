6e2c766ca90d4004689ccb82ff7425ba323efae0
Vamsi Addanki <vamsiaddanki666@gmail.com>
2026-01-18 00:01:13 -0500

    restricting search space, moving general synthesis to separate file

--- synthesis/synthesis.py
+++ synthesis/synthesis.py
@@ -2,6 +2,9 @@ from typing import List, Tuple, Dict
 import math
 import gurobipy as gp
 from gurobipy import GRB
+import random
+import time
+import os
 
 Pair = Tuple[int, int, int]
 Topology = Dict[Tuple[int, int], int]
@@ -38,6 +41,54 @@ class DPScheduler:
         self.logging = logging
         self._cache: Dict[Tuple[int, int], Tuple[Topology, float]] = {}
 
+        model = gp.Model()
+        model.Params.OutputFlag = self.logging
+        # model.Params.MIPFocus = 1
+        # model.Params.Heuristics = 0.5
+        # model.Params.Cuts = 1
+        # model.Params.NonConvex = 2
+        # model.Params.MIPGap = 0.05
+
+        # model.Params.PoolSearchMode = 2
+        # model.Params.PoolSolutions = 10
+        # model.Params.PoolGap = 1e10
+        model.Params.Threads = os.cpu_count()
+        # model.Params.TimeLimit = 2
+        # model.Params.Presolve = 2
+
+        I = list(range(self.s+1))
+
+        # Concurrent flow theta variable
+        # Lets give an epsilon for the lb, so that the inverse doesn't go to infinity
+        theta = model.addVars(I,lb=1e-6,ub=1,name="theta")
+        # auxiliary variable corresponding to the transmission time
+        T = model.addVars(I,lb=0,ub=20e6,name="T") # Limiting to 20 milliseconds for the transmission delay
+        # flow variable for ith step, for demand s,t, traversing edge u,v
+        flow_index = []
+        for i in range(1,self.s+1):
+            if self.logging:
+                print("adding flow indices", i)
+            for (s, t, _) in self.steps[i-1]:
+                for u in range(self.n):
+                    for v in range(self.n):
+                        if u != v:
+                            flow_index.append((i, s, t, u, v))
+
+        # print(flow_index.index((15,0,15,0,1)))
+        # exit()
+
+        if self.logging:
+            print("adding flow vars")
+        f = model.addVars(flow_index,lb=0,ub=self.d,name="f")
+        if self.logging:
+            print("finished adding flow vars")
+
+
+        self.model = model
+        self.T = T
+        self.theta = theta
+        self.f = f
+
     def ringNext(self,u, n):
         return (u + 1) % n
 
@@ -60,140 +111,112 @@ class DPScheduler:
 
     def completion_time(self, a: int, b: int) -> Tuple[Topology, float]:
         key = (a, b)
-        
+
+        ts = time.perf_counter_ns()
         if key in self._cache:
             return self._cache[key]
         else:
             if self.logging==1:
                 print(f'\n\n\n\n##### Solving for steps a={a}, b={b} #####')
 
-        model = gp.Model()
-        model.Params.OutputFlag = self.logging
-        model.Params.MIPFocus = 1
-        model.Params.Heuristics = 0.5
-        model.Params.Cuts = 1
-        model.Params.NonConvex = 2
-        model.Params.MIPGap = 0.1
-        model.Params.TimeLimit = 60
 
-
-        # x = {}
-        # for u in range(self.n):
-        #     for v in range(self.n):
-        #         if u != v:
-        #             # number of edges between u,v. This can also be interpreted as the capacity between u,v
-        #             if self.relaxation == 1:
-        #                 x[u, v] = model.addVar(vtype=GRB.CONTINUOUS, lb=0)
-        #             else:
-        #                 x[u, v] = model.addVar(vtype=GRB.INTEGER, lb=0)
         x = {}
-        for u in range(self.n):
-            for v in range(self.n):
-                if u == v:
-                    continue
+        z = {}
+        y = {}
+        topoSpace = 0
+        for i in range(1,self.s+1):
+        # for i in range(1,a+1):
+        #     if i>len(self.steps):
+        #         continue
+            topoSpace = topoSpace + 1
+            # direct connect topo corresponding to step i
+            y[i-1] = {}
+            for u in range(self.n):
+                for v in range(self.n):
+                    y[i-1][u,v]=0
+            for (s, t, demand) in self.steps[i - 1]:
+                # interpreted as the ideal number of links for direct transmission
+                y[i-1][s,t]=int(demand/self.chunksizes[i-1])
 
-                startVal = 0
+        print(f'\n\n\n\n##### Solving for steps a={a}, b={b} #####')
+        print("total = ", topoSpace)
 
-                if self.d == 1:
-                    if v == self.ringNext(u, self.n):
-                        startVal = 1
+        # ToDo: Could add other relevant topos to the topoSpace
 
-                elif self.d == 2:
-                    if v == self.ringNext(u, self.n) or v == self.ringPrev(u, self.n):
-                        startVal = 1
+        # for i in range(self.n):
+        #     for u in range(self.n):
+        #         for d in range(self.n):
+        #             y[i][u,(u+d)%n] = 1
 
-                elif self.d == 4:
-                    if v in self.torusNeighbors(u, self.dims):
-                        startVal = 1
+        model = self.model
+        T = self.T
+        theta = self.theta
+        f = self.f
 
-                if self.relaxation == 1:
-                    x[u, v] = model.addVar(vtype=GRB.CONTINUOUS, lb=0, name=f"x[{u},{v}]")
-                else:
-                    x[u, v] = model.addVar(vtype=GRB.INTEGER, lb=0, name=f"x[{u},{v}]")
+        objectiveValue = list()
+        for search in range(topoSpace):
+            tsprime = time.perf_counter_ns()
+            model.remove(model.getConstrs())
+            model.update()
+            
+            x = y[search]
 
-                x[u, v].Start = startVal
+            # Flow conservation and demand constraints
+            for i in range(a, b + 1): # For each step between a, b (including)
+                for (s, t, demand) in self.steps[i - 1]: # Note: Algorithm's steps are indexed from 1
+                    for u in range(self.n):
+                        outflow = gp.quicksum(f[i, s, t, u, v] for v in range(self.n) if u != v)
+                        inflow = gp.quicksum(f[i, s, t, v, u] for v in range(self.n) if u != v)
+                        if u == s:
+                            model.addConstr(outflow - inflow == theta[i]*int(demand/self.chunksizes[i-1]))
+                        elif u == t:
+                            model.addConstr(outflow - inflow == -theta[i]*int(demand/self.chunksizes[i-1]))
+                        else:
+                            model.addConstr(outflow - inflow == 0)
 
-
-        I = list(range(a, b + 1))
-        # Concurrent flow theta variable
-        # Lets give an epsilon for the lb, so that the inverse doesn't go to infinity
-        theta = model.addVars(I,lb=1e-6,ub=1,name="theta")
-        # auxiliary variable corresponding to the transmission time
-        T = model.addVars(I,lb=0,ub=20e6,name="T") # Limiting to 20 milliseconds for the transmission delay
-        # flow variable for ith step, for demand s,t, traversing edge u,v
-        flow_index = []
-        for i in range(a, b + 1):
-            if self.logging:
-                print("adding flow indices", i)
-            for (s, t, _) in self.steps[i - 1]:
+            # Capacity constraints
+            for i in range(a, b + 1):
                 for u in range(self.n):
                     for v in range(self.n):
                         if u != v:
-                            flow_index.append((i, s, t, u, v))
+                            model.addConstr(
+                                gp.quicksum(f[i, s, t, u, v] for (s, t, _) in self.steps[i - 1])
+                                <= x[u, v]
+                            )
 
-        if self.logging:
-            print("adding flow vars")
-        f = model.addVars(flow_index,lb=0,ub=self.d,name="f")
-        if self.logging:
-            print("finished adding flow vars")
+            for i in range(a, b + 1):
+                # We assume that m_i is same across all nodes within a single step,
+                # even in multi-port case i.e., same size sent on all ports
+                _bits = self.chunksizes[i-1]
+                # model.addQConstr(theta[i] * T[i] == self.beta * _bits)
+                # converting to soc constraint, see appendix
+                model.addQConstr((theta[i] - T[i]) * (theta[i] - T[i])+ 4.0 * self.beta * _bits<= (theta[i] + T[i]) * (theta[i] + T[i]))
 
-        model.update()
+            if self.relaxation == 1:
+                model.setObjective(gp.quicksum(self.alpha+T[i]*(1+ self.delta/(self.beta * self.chunksizes[i-1])) for i in range(a, b + 1)), GRB.MINIMIZE)
+            else:
+                model.setObjective(gp.quicksum(self.alpha+T[i]*(1+ self.delta/(self.beta * self.chunksizes[i-1])) for i in range(a, b + 1)), GRB.MINIMIZE)
+            model.optimize()
+            if model.Status != GRB.OPTIMAL:
+                cost = math.inf
+                objectiveValue.append(cost)
+            else:
+                objectiveValue.append(model.ObjVal)
+            
+            if self.logging:
+                print("instanceTime =",(time.perf_counter_ns()-tsprime)/1e9)
 
-        # Degree constraints
-        for u in range(self.n):
-            model.addConstr(gp.quicksum(x[u, v] for v in range(self.n) if u != v) == self.d)
-            model.addConstr(gp.quicksum(x[v, u] for v in range(self.n) if u != v) == self.d)
+        minIndex, minObj = min(enumerate(objectiveValue), key=lambda lam: lam[1])
+        topo: Topology = y[minIndex]
+        topo = {k: v for k, v in topo.items() if v != 0}
+        cost = minObj
+        if topo=={}:
+            print("###################")
+            assert(False)
 
-        # Flow conservation and demand constraints
-        for i in range(a, b + 1): # For each step between a, b (including)
-            for (s, t, demand) in self.steps[i - 1]: # Note: Algorithm's steps are indexed from 1
-                for u in range(self.n):
-                    outflow = gp.quicksum(f[i, s, t, u, v] for v in range(self.n) if u != v)
-                    inflow = gp.quicksum(f[i, s, t, v, u] for v in range(self.n) if u != v)
-                    if u == s:
-                        model.addConstr(outflow - inflow == theta[i]*int(demand/self.chunksizes[i-1]))
-                    elif u == t:
-                        model.addConstr(outflow - inflow == -theta[i]*int(demand/self.chunksizes[i-1]))
-                    else:
-                        model.addConstr(outflow - inflow == 0)
-
-        # Capacity constraints
-        for i in range(a, b + 1):
-            for u in range(self.n):
-                for v in range(self.n):
-                    if u != v:
-                        model.addConstr(
-                            gp.quicksum(f[i, s, t, u, v] for (s, t, _) in self.steps[i - 1])
-                            <= x[u, v]
-                        )
-
-        for i in range(a, b + 1):
-            # We assume that m_i is same across all nodes within a single step,
-            # even in multi-port case i.e., same size sent on all ports
-            _bits = self.chunksizes[i-1]
-            model.addQConstr(theta[i] * T[i] == self.beta * _bits)
-
-        model.setObjective(gp.quicksum(self.alpha+T[i]*(1+ self.delta/(self.beta * self.chunksizes[i-1])) for i in range(a, b + 1)), GRB.MINIMIZE)
-        model.optimize()
-
-        if model.Status != GRB.OPTIMAL:
-            topo = {}
-            # if model.Status == GRB.INFEASIBLE:
-            #     print("Infeasible model. Check the constraints")
-            cost = math.inf
-            self._cache[key] = (topo, cost)
-            return topo, cost
-
-        topo: Topology = {}
-        for (u, v), var in x.items():
-            val = var.X
-            if val - math.floor(val) > 0.5:
-                topo[(u, v)] = int(math.ceil(val))
-            elif val>=1:
-                topo[(u,v)] = int(math.floor(val))
-
-        cost = model.ObjVal
         self._cache[key] = (topo, cost)
+        if self.logging:
+            print("time=",(time.perf_counter_ns()-ts)/1e9)
         return topo, cost
 
 
@@ -214,6 +237,7 @@ class DPScheduler:
         DP[self.s + 1][k] = 0.0
 
         for t in range(1, k + 1):
+            # print(f"solving for t={t}")
             for a in range(1, self.s + 1):
                 best = math.inf
                 best_b = None
@@ -256,6 +280,8 @@ class DPScheduler:
         best_cost = math.inf
         best_schedule: List[Tuple[Topology, int]] = []
         for k in range(0, self.s + 1):
+            if self.logging:
+                print(f"###### Solving for {k} reconfigurations")
             cost_no_reconf, sched = self.synthesize_for_k(k)
             # print(k,sched)
             total_cost = cost_no_reconf + k * self.alpha_r
@@ -264,6 +290,7 @@ class DPScheduler:
                 best_cost = total_cost
                 best_schedule = sched
         return best_cost, best_schedule
+
 
     def expandSchedulePerStep(self,
         schedule: List[Tuple[Topology, int]],
