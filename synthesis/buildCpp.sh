d68ef85680d0810b415b533ac65ece87064cf63b
Vamsi Addanki <vamsiaddanki666@gmail.com>
2026-02-01 21:25:32 -0500

    version

--- synthesis/buildCpp.sh
+++ synthesis/buildCpp.sh
@@ -2,4 +2,4 @@
 
 # CXXFLAGS="-g -O0 -fno-omit-frame-pointer -fno-fast-math -std=c++17"
 CXXFLAGS="-O3 -std=c++17"
-g++ $CXXFLAGS -I$GUROBI_HOME/include/ -I./   main.cc DPSchedulerPath.cc -L$GUROBI_HOME/lib/ -lgurobi_c++ -lgurobi120 -o synthesize-schedule
+g++ $CXXFLAGS -I$GUROBI_HOME/include/ -I./   main.cc DPSchedulerPath.cc -L$GUROBI_HOME/lib/ -lgurobi_c++ -lgurobi130 -o synthesize-schedule
